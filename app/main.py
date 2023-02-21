8129b92a04f949030a51b33e99ba33dba7adda4d
Yoni Limo <limzyon@gmail.com>
2023-02-21 20:30:54 +0300

    updated main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
+from .db import get_db
+from sqlalchemy.orm import Session
+import models
 
 
 app = FastAPI()
@@ -8,3 +10,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "The pathway to your internship future."}
+
+@app.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.Users).all()
+    print(users)
+    return users
