b9292c81a867fbe1474691323167a4d82c24fbff
Yoni Limo <limzyon@gmail.com>
2023-02-21 20:31:14 +0300

    query psycopg2

--- app/db.py
+++ app/db.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+# import psycopg2
+# from psycopg2.extras import RealDictCursor
+import time
 
 #still need to abstract the specifics
-db_url = "postgres://postgres:password@localhost:5432/ballot"
+db_url = "postgresql://postgres:password@localhost:5432/ballot"
 
 engine = create_engine(db_url) 
 
@@ -17,3 +20,15 @@ def get_db():
         yield db
     finally: 
         db.close() 
+
+
+# while True:
+#     try:
+#         conn = psycopg2.connect(host="localhost", database="fastapi", user="postgres", password="password", cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Succesful connection.")
+#         break
+#     except Exception as error:
+#         print("Database connection failed")
+#         print(error)
+#         time.sleep(2)
