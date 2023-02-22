51a452ef0a72d52be805bd23d69025351558a9a0
Yoni Limo <limzyon@gmail.com>
2023-02-22 17:25:30 +0300

    added schemas for users

---
+++ app/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+
+
+class UserCreate(BaseModel):
+    reg_num: str
+    name: str
+    email: EmailStr
+    password: str
+
+class UserReturn(BaseModel):
+    id: str
+    reg_num: str
+    email: str
+
+    class Config:
+        orm_mode = True
