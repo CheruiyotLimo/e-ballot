d87d91c89be2ddc8bbd64f01653365d5c5e2914e
Yoni Limo <limzyon@gmail.com>
2023-02-23 16:19:59 +0300

    updated the router pointers

--- app/main.py
+++ app/main.py
@@ -1,38 +1,16 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
 from .db import engine, get_db
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
+from .routers import auth, users
 
 
 app = FastAPI()
 
+app.include_router(users.router)
+app.include_router(auth.router)
 
 @app.get("/")
 async def root():
     return {"message": "The pathway to your internship future."}
 
-@app.get("/users/{id}", response_model=schemas.UserReturn)
-def get_users(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.Users).filter(models.Users.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user with id {id} doesn,t exist")
-    print(user)
-    return user
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserReturn)
-def register_user(data: schemas.UserCreate, db: Session = Depends(get_db)):
-    user_email = db.query(models.Users).filter(models.Users.email == data.email)
-    user_reg_no = db.query(models.Users).filter(models.Users.reg_num == data.reg_num)
-
-    if user_email.first() or user_reg_no.first():
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="A user already exists with similar email or registration number!!")
-    
-    new_password = utils.hash(data.password)
-    data.password = new_password
-    data = models.Users(**data.dict())
-
-    db.add(data)
-    db.commit()
-    db.refresh(data)
-    return data
