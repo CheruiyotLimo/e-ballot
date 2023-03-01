32de412911737988ccc0a3bc32a5f57961c94125
Yoni Limo <limzyon@gmail.com>
2023-03-01 12:20:58 +0300

    included hosp router

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
 from .db import engine, get_db
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
-from .routers import auth, users
+from .routers import auth, users, hosp
 
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(hosp.router)
+
 
 @app.get("/")
 async def root():
