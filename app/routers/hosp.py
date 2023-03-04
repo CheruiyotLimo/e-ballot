40b326ffd6fd7f56767897c6f3d16fe06fd26e63
Yoni Limo <limzyon@gmail.com>
2023-03-04 14:23:23 +0300

    added user can chose hospital

--- app/routers/hosp.py
+++ app/routers/hosp.py
@@ -24,3 +24,29 @@ def get_all_hospitals(current_user: int = Depends(oauth2.get_current_user), db: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There are currently no open hospitals.")
 
     return hosps
+
+@router.post("/choice", status_code=status.HTTP_201_CREATED)
+def choose_hospital(hospital_id: int, current_user: int = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
+    """POST request for a user to declare a hospital of their choice for placement."""
+    
+    print("123")
+    # Query hosp database for the specified choice
+    choice = db.query(models.Hospital).filter(models.Hospital.id == hospital_id).first()
+
+    if not choice:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no hospital with id {hospital_id}")
+    print("tou")
+    # Get current active user
+    # user = db.query(models.Users).filter(models.Users.email == current_user.email).first()
+
+    # Update their choices column
+    current_user.choice = choice.id
+    print("me")
+    # Refresh the database
+    db.commit()
+    db.refresh(current_user)
+
+    return f"Your chose {choice}"
+
+  
+
