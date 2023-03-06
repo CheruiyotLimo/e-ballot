392961e1921d40fb630a1a53c11658e048375b8a
Yoni Limo <limzyon@gmail.com>
2023-03-06 17:33:53 +0300

    Added post hospital route

--- app/routers/hosp.py
+++ app/routers/hosp.py
@@ -25,28 +25,48 @@ def get_all_hospitals(current_user: int = Depends(oauth2.get_current_user), db: 
 
     return hosps
 
-@router.post("/choice", status_code=status.HTTP_201_CREATED)
-def choose_hospital(hospital_id: int, current_user: int = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
+@router.put("/choice", status_code=status.HTTP_201_CREATED)
+def choose_hospital(hosp_id: int, current_user: int = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
     """POST request for a user to declare a hospital of their choice for placement."""
-    
+
     print("123")
     # Query hosp database for the specified choice
-    choice = db.query(models.Hospital).filter(models.Hospital.id == hospital_id).first()
+    choice = db.query(models.Hospital).filter(models.Hospital.id == hosp_id).first()
 
     if not choice:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no hospital with id {hospital_id}")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no hospital with id {hosp_id}")
     print("tou")
     # Get current active user
-    # user = db.query(models.Users).filter(models.Users.email == current_user.email).first()
+    user = db.query(models.Users).filter(models.Users.email == current_user.email)
 
     # Update their choices column
-    current_user.choice = choice.id
+    user.update({"choices": choice.id}, synchronize_session=False)  
     print("me")
     # Refresh the database
     db.commit()
-    db.refresh(current_user)
+    db.refresh(user)
 
     return f"Your chose {choice}"
 
   
+@router.post("/", response_model=schemas.HospReturn, status_code=status.HTTP_201_CREATED)
+def add_hospital(hosp: schemas.HospCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    """ POST request for updating the hospital database"""
 
+    # Check if user is authorized ## Will come to add admin requirement later
+    user = db.query(models.Users).filter(models.Users.email == current_user.email).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not authorized to make this change.")
+    
+    #C
+    
+    # Convert the dictionary to a model dictionary
+    new_hosp = models.Hospital(**hosp.dict())
+
+    # Add new hospital to the database
+    db.add(new_hosp)
+    db.commit()
+    db.refresh(new_hosp)
+
+    return hosp
