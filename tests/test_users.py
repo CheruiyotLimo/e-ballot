6a0c59a0e2878494d15ad284ab57cc3abea58080
Yoni Limo <limzyon@gmail.com>
2023-03-13 17:16:20 +0300

    testing add user route

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,15 @@
 from fastapi.testclient import TestClient
-from app import main
+from app import main, schemas
 
 client = TestClient(main.app)
 
 def test_root():
     res = client.get("/")
-    print(res)
+    print(res.json())
+    assert res.status_code == 200
+
+def test_add_user():
+    res = client.post("/users/", json={"reg_num": "H31/2000/1298", "name": "Fraser", "email": "q123@gmail.com", "password": "password123"})
+    test_user = schemas.UserReturn(**res.json())
+    assert test_user.email == "q123@gmail.com"
+    assert res.status_code == 201
