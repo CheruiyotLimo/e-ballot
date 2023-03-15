2a0cbc0df96b7e62784212c78f8f97489568adbf
Yoni Limo <limzyon@gmail.com>
2023-03-15 16:56:38 +0300

    added admin router

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
 from .db import engine, get_db
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
-from .routers import auth, users, hosp
+from .routers import auth, users, hosp, admin
 
 
 app = FastAPI()
@@ -10,6 +10,7 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(hosp.router)
+app.include_router(admin.router)
 
 
 @app.get("/")
