ad5ee0322a4704cae1563a81e8bcd382f11c2638
Yoni Limo <limzyon@gmail.com>
2023-03-15 16:56:27 +0300

    added randomizer script

--- Scripts/randomizer.py
+++ Scripts/randomizer.py
@@ -1,16 +1,14 @@
 import random
 
-def randomn():
-    return random.randint(1, 10)
 
+def randomizer():
+    checked = set()
+    user_list = 10
 
-def randomizer(list1):
-    checker = set()
-    for i in range(len(list1)):
-        r = random.choice(list1)
-        if r not in checker:
-            checker.add(r)
-            print(r)
-
-# print(randomn())
-randomizer([1, 2, 3, 4, 6, 5, 7, 8, 9])
+    while len(checked) < user_list:
+        rand = random.randint(1, user_list)
+        if rand in checked:
+            continue
+        else:
+            checked.add(rand)
+            yield rand
