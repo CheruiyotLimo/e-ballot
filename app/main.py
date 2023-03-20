fd893e8a29b15ddf7f2fe81a2c187d616ae1d06b
Yoni Limo <limzyon@gmail.com>
2023-03-20 20:41:11 +0300

    added main update

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from .db import engine, get_db
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .routers import auth, users, hosp, admin
-
+import requests
 
 app = FastAPI()
 
@@ -16,4 +16,14 @@ app.include_router(admin.router)
 @app.get("/")
 async def root():
     return {"message": "The pathway to your internship future."}
+
+
+# @app.get("/call-root")
+# async def call_root():
+#     # response = await root()
+
+#     url = "http://127.0.0.1:8000/hosps"
+#     results = requests.get(url)
+#     print(results)
+#     return {"response": results}
 
