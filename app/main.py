4ee7bcfd5a2ecc927aae1703111dd15de1e2d575
Yoni Limo <limzyon@gmail.com>
2023-03-25 15:01:33 +0300

    mastered

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
 from .db import engine, get_db
 from sqlalchemy.orm import Session
-from . import models, schemas, utils
+from . import models, schemas, utils, oauth2
 from .routers import auth, users, hosp, admin
 import requests
-
+from typing import Optional
 app = FastAPI()
 
 app.include_router(users.router)
@@ -18,12 +18,7 @@ async def root():
     return {"message": "The pathway to your internship future."}
 
 
-# @app.get("/call-root")
-# async def call_root():
-#     # response = await root()
-
-#     url = "http://127.0.0.1:8000/hosps"
-#     results = requests.get(url)
-#     print(results)
-#     return {"response": results}
-
+@app.get("/call-root")
+async def call_root(current_user: int = Depends(oauth2.get_current_user), db: Session = (Depends(get_db)), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    return hosp.get_all_hospitals(current_user, db)
+    return "Success"
