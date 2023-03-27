9335d4d4c6ec16a83000c386d5b32ac4a8330bf0
Yoni Limo <limzyon@gmail.com>
2023-03-27 17:23:33 +0300

    updated authentication flow

--- gmail/quickstart.py
+++ gmail/quickstart.py
@@ -9,7 +9,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send']
 
 
 def main():
