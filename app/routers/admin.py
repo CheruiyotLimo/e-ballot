a3db35469274fccc0d939ae87473cf7733c54660
Yoni Limo <limzyon@gmail.com>
2023-03-28 21:28:25 +0300

    modded first pass admin flow

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -45,24 +45,29 @@ def first_round(db: Session = Depends(get_db), current_user: int = Depends(oauth
 
         # Query the hospital db for the hospital
         hospital = hosp.get_one_hospital(id=user_choice, current_user=current_user, db=db)
-
-        if hospital.slots >= 1:
-
-            # Update hospital slots
-            new_slots = {"slots": hospital.slots-1}
-            json_obj = json.dumps(new_slots)
-            new_slots = json.loads(json_obj)
-            hosp.patch_hospital_slots(hosp=new_slots, hosp_id=user_choice, current_user=current_user, db=db)
-            
-            # Update the new assigned hospital db
-            allocated_user = {"name": user.first().name, "hosp_name": hospital.name}
-            final.add_to_final_list(entry=allocated_user, current_user=current_user, db=db)
-
-            print(f"Allocated {allocated_user['name']} to {allocated_user['hosp_name']}")
+        
+        if not hospital:
+            deprecated_choice = {"choice": None}
+            users.patch_user(user_data=deprecated_choice, user_id=user.first().id, current_user=current_user, db=db)
+        
         else:
-            print("Hospital is out of slots.")
-            # Remove hosp from the db
-            # hosp.delete_hospital(hosp_id=user_choice, current_user=current_user, db=db)
+            if hospital.slots >= 1:
+
+                # Update hospital slots
+                new_slots = {"slots": hospital.slots-1}
+                json_obj = json.dumps(new_slots)
+                new_slots = json.loads(json_obj)
+                hosp.patch_hospital_slots(hosp=new_slots, hosp_id=user_choice, current_user=current_user, db=db)
+                
+                # Update the new assigned hospital db
+                allocated_user = {"name": user.first().name, "hosp_name": hospital.name}
+                final.add_to_final_list(entry=allocated_user, current_user=current_user, db=db)
+
+                print(f"Allocated {allocated_user['name']} to {allocated_user['hosp_name']}")
+            else:
+                print(f"{hospital.name} is out of slots.")
+                # Remove hosp from the db
+                # hosp.delete_hospital(hosp_id=user_choice, current_user=current_user, db=db)
            
         
         
