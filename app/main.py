05045c5228279f43312cc9e01745b1a9cb866139
Yoni Limo <limzyon@gmail.com>
2023-04-25 21:43:23 +0300

    updated call root endpoint

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from . import models, schemas, utils, oauth2
 from .routers import auth, users, hosp, admin, final
 import requests
 from typing import Optional
+
 app = FastAPI()
 
 app.include_router(users.router)
@@ -21,5 +22,10 @@ async def root():
 
 @app.get("/call-root")
 async def call_root(current_user: int = Depends(oauth2.get_current_user), db: Session = (Depends(get_db)), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
-    return hosp.get_all_hospitals(current_user, db)
-    return "Success"
+    
+    choice_1 = {"first_choice": 5}
+    choice_2 = {"first_choice": 2}
+
+    return users.patch_user(user_data=choice_1, user_id=26, current_user=current_user, db=db)
+
+    return "Success!!!!!"
