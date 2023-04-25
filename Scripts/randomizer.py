15e3968d608940bf30542357dfa1e76c652ff7c7
Yoni Limo <limzyon@gmail.com>
2023-04-25 21:49:56 +0300

    randomizer change

--- Scripts/randomizer.py
+++ Scripts/randomizer.py
@@ -4,10 +4,10 @@ import random
 def randomizer():
     checked = set()
     user_list = 20
-    rand_list = []
+    rand_list = [14, 21, 22, 23, 24, 26, 27]
 
-    while len(rand_list) < 4:
-        rand = random.randint(21, 24)
+    while len(rand_list) < 6:
+        rand = random.randint(21, 27)
         if rand in checked:
             continue
         else:
