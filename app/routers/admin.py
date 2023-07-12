e92d20158ee234fb7d20e302e9f6b5165f5a834b
Yoni Limo <limzyon@gmail.com>
2023-07-12 14:31:20 +0300

    reworked sorting logic

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -13,6 +13,7 @@ from . import hosp, users, auth, final
 import json, time
 from typing import Literal
 from sqlalchemy.sql.expression import func
+import random
 
 router = APIRouter(
     prefix="/admin",
@@ -39,13 +40,19 @@ def first_round(select_round: Rounds, db: Session = Depends(get_db), current_use
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No registered users exist")
 
     # Access user ids from randomizer scripts
-    for id in randomizer.randomizer():
+    # Need a statement to loop through the options.
+    
+    # while user_list:
+    for id in randomizer.new_rand(user_list):  # Bug is in this randomizing logic
+
         user = db.query(models.Users).filter(models.Users.id == id)
 
-        print(id)
+        print(id) 
         if not user.first():
             continue
 
+        if not user.first().first_choice:
+            continue
 
         # Check user choice based on round of selection
         if select_round == 1:
@@ -60,7 +67,7 @@ def first_round(select_round: Rounds, db: Session = Depends(get_db), current_use
         # If hospital has no slots, nullify user's choice
             
     
-        if hospital.slots >= 1:
+        if hospital and hospital.slots >= 1:
             # Update hospital slots
             new_slots = {"slots": hospital.slots-1}
             json_obj = json.dumps(new_slots)
@@ -84,12 +91,12 @@ def first_round(select_round: Rounds, db: Session = Depends(get_db), current_use
             # Remove hosp from the db
             # hosp.delete_hospital(hosp_id=user_choice, current_user=current_user, db=db)
         time.sleep(1)   
-        
-        
-        # user.delete(synchronize_session=False)
-        # else:
-        #     # Remove hosp from db if slots
     
+    
+    # user.delete(synchronize_session=False)
+    # else:
+    #     # Remove hosp from db if slots
+
     
     return user_list
 
@@ -103,10 +110,12 @@ def final_pass(current_user: int = Depends(oauth2.get_current_user), db: Session
 
     # Needs a loop to account for all unselected applicants
     all_users = db.query(models.Users).filter(models.Users.role == None, models.Users.first_choice != None, models.Users.second_choice!=None).all()
+    print(len(all_users))
     while all_users:
         # select a random user
-        user = db.query(models.Users).order_by(func.random())
-
+        # user = db.query(models.Users).order_by(func.random())
+        user = all_users[random.randint(0, len(all_users))-1]
+        # print(user.first())
         # select a random hospital
         hospital = db.query(models.Hospital).order_by(func.random())
 
@@ -118,11 +127,11 @@ def final_pass(current_user: int = Depends(oauth2.get_current_user), db: Session
             hosp.patch_hospital_slots(hosp=new_slots, hosp_id=hospital.first().id, current_user=current_user, db=db)
             
             # Update the new assigned hospital db
-            allocated_user = {"name": user.first().name, "hosp_name": hospital.first().name}
+            allocated_user = {"name": user.name, "hosp_name": hospital.first().name}
             final.add_to_final_list(entry=allocated_user, current_user=current_user, db=db)
 
             # Delete the user from the all_users list
-            all_users.remove(user.first())        
+            all_users.remove(user)       
         else:
             print(f"{hospital.first().name} is out of slots.")
 
