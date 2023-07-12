01039a314edda7d8916ca85ac749710b0d3cf7fe
Yoni Limo <limzyon@gmail.com>
2023-07-12 14:31:52 +0300

    reworked randomizer logic

--- Scripts/randomizer.py
+++ Scripts/randomizer.py
@@ -1,19 +1,27 @@
 import random
 
 
-def randomizer():
-    checked = set()
-    user_list = 20
+# def randomizer(rand_list):
+#     checked = set()
+#     user_list = 20
+#     rand_list = [14, 21, 22, 23, 24, 26, 27]
+    
+#     rand = random.shuffle(rand_list)[0]
+#     rand_list.remove(rand)
+
+#     # while len(rand_list) < 6:
+#     #     rand = random.randint(21, 27)
+#     #     if rand in checked:
+#     #         continue
+#     #     else:
+#     #         checked.add(rand)
+#     #         rand_list.append(rand)
+
+#     return rand
+
+def new_rand(applicants_list: list):
     rand_list = [14, 21, 22, 23, 24, 26, 27]
-
-    while len(rand_list) < 6:
-        rand = random.randint(21, 27)
-        if rand in checked:
-            continue
-        else:
-            checked.add(rand)
-            rand_list.append(rand)
-
+    random.shuffle(rand_list)
     return rand_list
 
-print(randomizer())
+# print(randomizer())
